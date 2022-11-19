859b1ee8863ab8297652af658c416dfb33c23af6
sleek1004 <arojojoyeo@yahoo.com>
2022-11-19 23:02:42 +0100

    using checkbox

--- src/Form.js
+++ src/Form.js
@@ -2,16 +2,23 @@ import React from 'react'
 
 export default function Form(){
     const[formData, setFormData] = React.useState(
-        {firstName: "", lastName :"", email:"", comment:""}
+        
+        {firstName: "", 
+        lastName :"", 
+        email: "", 
+        comment: "",
+       isFriendly: true
+    }
         )
     
 
 
     function handleChanged(event){
+        const{name, value}= event.target
        setFormData(prevFormData => {
         return{
             ...prevFormData,
-            [event.target.name]:event.target.value
+          [ name]:value 
         }
        })
     }
@@ -49,6 +56,14 @@ export default function Form(){
             value={formData.comment}
             name="comment"
              />
+             <input
+             type="checkbox"
+             id="isFriendly"
+             checked={formData.isFriendly}
+             onChange={handleChanged}
+             />
+             <label htmlFor='isFriendly'> Are you friendly?</label>
+             <br/>
         </form>
         
     )
